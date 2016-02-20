a5a40dd7cd03bf6d90fd37ecc6bb09aa7dfa7f59
Alexander Ross <alex@aross.se>
2016-02-20 19:06:20 +0100

    Update policy for bookings

--- app/policies/booking_policy.rb
+++ app/policies/booking_policy.rb
@@ -3,6 +3,20 @@ class BookingPolicy < ApplicationPolicy
     true
   end
 
+  def update?
+    super || record.restaurant == user&.restaurant
+  end
+
+  def permitted_attributes
+    base_attributes = [:name, :phone_number, :number_of_persons, :starts_at]
+
+    if user.nil?
+      base_attributes
+    else
+      base_attributes + [:state]
+    end
+  end
+
   class Scope < Scope
     def resolve
       scope
