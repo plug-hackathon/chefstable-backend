1ef13dfa60b2e1ca73a92f88312bee785fe0beb1
Mikael Lisselgard <mikael.lisselgard@hotmail.se>
2016-02-20 17:57:28 +0000

    Add restaurant tests

---
+++ test/controllers/restaurants_controller_test.rb
@@ -0,0 +1,22 @@
+require 'test_helper'
+
+class RestaurantsControllerTest < ActionController::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+  def test_create_restaurant
+  	assert_difference "Restaurant.count" do
+  		post :create, restaurant: {
+  			name: "Smak",
+  			time_zone: "0100"
+  		}, format: :json
+  		assert_response :success
+  	end 
+
+  	assert_equal "", response.body
+
+  	restaurant = assigns(:restaurant)
+  	assert_equal "Smak", restaurant.name
+  	assert_equal "0100", restaurant.time_zone
+  end
+end
