dc29f1f73b06d17e54c32f934b8949f6fd4fc8ef
Mikael Lisselgård <mikael.lisselgard@hotmail.se>
2016-02-20 21:11:31 +0000

    Move test to correct model

--- test/models/opening_hour_test.rb
+++ test/models/opening_hour_test.rb
@@ -1,7 +1,27 @@
 require 'test_helper'
 
 class OpeningHourTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  
+  def test_valid_from_time
+  	openinghour = OpeningHour.new
+  	openinghour.from_time = "18:00"
+  	openinghour.to_time = "22:00"
+  	assert openinghour.valid?
+
+  	openinghour.from_time = "fb:kk"
+  	refute openinghour.valid? 
+  	assert_includes openinghour.errors.keys, :from_time
+  end
+
+  def test_valid_to_time 
+  	openinghour = OpeningHour.new
+  	openinghour.from_time = "18:00"
+  	openinghour.to_time = "22:00"
+  	assert openinghour.valid?
+
+  	openinghour.to_time = "fb:kk"
+  	refute openinghour.valid? 
+  	assert_includes openinghour.errors.keys, :to_time
+  end
+  
 end
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,27 +1,4 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
- 
-  def test_valid_from_time
-  	openinghour = OpeningHour.new
-  	openinghour.from_time = "18:00"
-  	openinghour.to_time = "22:00"
-  	assert openinghour.valid?
-
-  	openinghour.from_time = "fb:kk"
-  	refute openinghour.valid? 
-  	assert_includes openinghour.errors.keys, :from_time
-  end
-
-  def test_valid_to_time 
-  	openinghour = OpeningHour.new
-  	openinghour.from_time = "18:00"
-  	openinghour.to_time = "22:00"
-  	assert openinghour.valid?
-
-  	openinghour.to_time = "fb:kk"
-  	refute openinghour.valid? 
-  	assert_includes openinghour.errors.keys, :to_time
-  end
-
 end
