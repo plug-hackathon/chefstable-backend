1bb3fa6d0162d58884ebfe25ad46b296c5882f83
Mikael Lisselgård <mikael.lisselgard@hotmail.se>
2016-02-20 21:51:09 +0000

    Fix typo and validate week day enum

--- app/models/opening_hour.rb
+++ app/models/opening_hour.rb
@@ -1,9 +1,10 @@
 class OpeningHour < ActiveRecord::Base
 	TIME_FORMAT = %r(\A\d{2}:\d{2}\z)
-	enum state: %w( Monday Tuesday Wednesday Thursday Friday Saturday Sunday )
+	enum week_day: %w( Monday Tuesday Wednesday Thursday Friday Saturday Sunday )
   
   belongs_to :restaurant
 
+  validates :week_day, inclusion: { in: week_days.keys }
   validates :from_time, format: { with: TIME_FORMAT }
   validates :to_time, format: { with: TIME_FORMAT }
 end
