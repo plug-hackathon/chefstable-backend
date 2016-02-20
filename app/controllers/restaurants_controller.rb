a266119383965e109efed3b3b0769d851d17c22e
Alexander Ross <alex@aross.se>
2016-02-20 19:55:35 +0100

    Tabs to spaces

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,49 +1,48 @@
 class RestaurantsController < ApplicationController
-	before_action :set_post, except: %i(index create)
+  before_action :set_post, except: %i(index create)
 
-	def index
-		@restaurants = policy_scope(Restaurant)
-		render json: @restaurants
-	end 
+  def index
+    @restaurants = policy_scope(Restaurant)
+    render json: @restaurants
+  end 
 
-	def show 
-		authorize @restaurant
-		render json: @restaurant	
-	end
+  def show 
+    authorize @restaurant
+    render json: @restaurant
+  end
 
-	def create
-		@restaurant = Restaurant.new(restaurant_params)
-		authorize @restaurant
-		if @restaurant.save
-			render :json
-		else
-			render json: { errors: @restaurant.errors }, status: :bad_request
-		end
-	end
+  def create
+    @restaurant = Restaurant.new(restaurant_params)
+    authorize @restaurant
+    if @restaurant.save
+      render json: @restaurant
+    else
+      render json: { errors: @restaurant.errors }, status: :bad_request
+    end
+  end
 
-	def update 
-		authorize @restaurant
-		@restaurant.attributes = restaurant_params
-		authorize @restaurant
+  def update 
+    authorize @restaurant
+    @restaurant.attributes = restaurant_params
+    authorize @restaurant
 
-		if @restaurant.save
-			render :json
-		else 
-			render json: { errors: @restaurant.errors }, status: :bad_request
-		end
-	end
+    if @restaurant.save
+      render json: @restaurant
+    else 
+      render json: { errors: @restaurant.errors }, status: :bad_request
+    end
+  end
 
-	def destroy
-		authorize @restaurant
-		head (@restaurant.destroy ? :ok : :bad_request)
-	end
+  def destroy
+    authorize @restaurant
+    head (@restaurant.destroy ? :ok : :bad_request)
+  end
 
-	private def restaurant_params 
-		params.require(:restaurant).permit(:name, :time_zone)
-	end
+  private def restaurant_params 
+    params.require(:restaurant).permit(:name, :time_zone)
+  end
 
-	private def set_restaurant 
-		@restaurant = Restaurant.find(params[:id])
-	end 
-
+  private def set_restaurant 
+    @restaurant = Restaurant.find(params[:id])
+  end 
 end
