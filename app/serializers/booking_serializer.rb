23f2d825eef52b1b29903cf9cae7852c3a2a9f1d
Mikael Lisselgård <mikael.lisselgard@hotmail.se>
2016-02-21 14:41:58 +0000

    add email and message to booking serializer

--- app/serializers/booking_serializer.rb
+++ app/serializers/booking_serializer.rb
@@ -1,3 +1,3 @@
 class BookingSerializer < ActiveModel::Serializer
-  attributes :id, :name, :phone_number, :number_of_persons, :starts_at, :state
+  attributes :id, :name, :phone_number, :number_of_persons, :starts_at, :state, :email, :message
 end
