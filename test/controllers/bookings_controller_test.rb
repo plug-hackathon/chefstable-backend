796f8efa8acc96cf2cb40fed636a6e41cc8d6080
Mikael Lisselgård <mikael.lisselgard@hotmail.se>
2016-02-21 11:06:32 +0000

    Add email to test

--- test/controllers/bookings_controller_test.rb
+++ test/controllers/bookings_controller_test.rb
@@ -6,6 +6,7 @@ class BookingsControllerTest < ActionController::TestCase
       post :create, booking: {
         name: "Mikael Anka",
         phone_number: "070 123 45 87",
+        email: "test@example.com",
         number_of_persons: 4,
         starts_at: "2016-09-20 15:00:00 +0100",
         restaurant_id: restaurants(:one).id
@@ -16,6 +17,7 @@ class BookingsControllerTest < ActionController::TestCase
     booking = assigns(:booking)
     assert_equal "pending", booking.state
     assert_equal "Mikael Anka", booking.name
+    assert_equal "test@example.com", booking.email
     assert_equal "070 123 45 87", booking.phone_number
     assert_equal 4, booking.number_of_persons
     assert_equal "2016-09-20 15:00:00 +0100".to_time, booking.starts_at
