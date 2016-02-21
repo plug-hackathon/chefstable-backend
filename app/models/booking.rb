da94c2f96e028c722802dd22fa51fbe6bde71690
Mikael Lisselgård <mikael.lisselgard@hotmail.se>
2016-02-21 11:06:39 +0000

    Add validations

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,8 +1,14 @@
 class Booking < ActiveRecord::Base
+	EMAIL_FORMAT = %r(\A\S+@\S+\z)
   enum state: %w( pending accepted rejected cancelled )
 
   belongs_to :restaurant
 
   validates :state, inclusion: { in: states.keys }
   validates :restaurant_id, presence: true
+  validates :number_of_persons, presence: true
+  validates :phone_number, presence: true
+  validates :starts_at, presence: true
+  validates :email, format: { with: EMAIL_FORMAT }
+
 end
